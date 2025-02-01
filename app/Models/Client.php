84ee250208a53a7884821a5524f4c455a767b9f2
lucalana <lucalana13@gmail.com>
2025-02-01 15:35:35 -0300

    Criando tabela, models e relacionamentos

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Client extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function signature(): HasOne
+    {
+        return $this->hasOne(Signature::class);
+    }
 }
---
+++ app/Models/Signature.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Signature extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'client_id',
+        'plan_id',
+        'status'
+    ];
+
+    protected $casts = [
+        'status' => SignatureStatus::class
+    ];
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function plan(): BelongsTo
+    {
+        return $this->belongsTo(Plan::class);
+    }
+}
