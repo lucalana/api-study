b6f54c04e64de92370c02af8b9186b6a7de0dd46
lucalana <lucalana13@gmail.com>
2025-02-02 16:07:49 -0300

    Criando rota de login e protegendo acessos de token

--- app/Http/Requests/StoreClientRequest.php
+++ app/Http/Requests/StoreClientRequest.php
@@ -23,7 +23,7 @@ class StoreClientRequest extends FormRequest
     {
         return [
             'name' => ['required'],
-            'email' => ['required', 'email'],
+            'email' => ['required', 'email', 'unique:users,email'],
             'password' => ['required']
         ];
     }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('client', ClientController::class);
+Route::post('/login', LoginController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::apiResource('client', ClientController::class);
+
+});
