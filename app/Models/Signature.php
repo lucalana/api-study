7f261e3c8df1e6bdcd209dabfbf9d41db3e6996c
lucalana <lucalana13@gmail.com>
2025-02-04 20:27:10 -0300

    Diminuindo quantidade de seed e adicionando import

--- app/Models/Signature.php
+++ app/Models/Signature.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\SignatureStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
